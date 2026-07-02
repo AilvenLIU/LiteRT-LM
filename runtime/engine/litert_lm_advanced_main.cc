903c9bbbe3429c0af8de247eb48dec174a80b2bd
Bangcheng Wang <bcwang@google.com>
2026-07-01 23:48:10 -0700

    [litertlm] Add --no_repeat_ngram_size` and `--no_repeat_ngram_window_size flags to LiteRT LM.
    LiteRT-LM-PiperOrigin-RevId: 941505128

--- runtime/engine/litert_lm_advanced_main.cc
+++ runtime/engine/litert_lm_advanced_main.cc
@@ -28,6 +28,7 @@
 #include <set>
 #include <sstream>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "absl/container/flat_hash_set.h"  // from @com_google_absl
@@ -41,6 +42,7 @@
 #include "absl/strings/str_cat.h"  // from @com_google_absl
 #include "absl/strings/str_split.h"  // from @com_google_absl
 #include "absl/strings/string_view.h"  // from @com_google_absl
+#include "runtime/components/logits_processor/no_repeat_ngram_config.h"
 #include "runtime/components/logits_processor/repetition_penalty_config.h"
 #include "runtime/components/logits_processor/suppress_tokens_config.h"
 #include "runtime/engine/litert_lm_lib.h"
@@ -136,19 +138,25 @@ litert::lm::RepetitionPenaltyConfig GetRepetitionPenaltyConfig() {
       absl::GetFlag(FLAGS_repetition_window_size));
 }
 
+litert::lm::NoRepeatNgramConfig GetNoRepeatNgramConfig() {
+  return litert::lm::NoRepeatNgramConfig(
+      /*no_repeat_ngram_size=*/absl::GetFlag(FLAGS_no_repeat_ngram_size),
+      /*window_size=*/absl::GetFlag(FLAGS_no_repeat_ngram_window_size));
+}
+
 ::litert::lm::SuppressTokensConfig GetSuppressTokensConfig(
     absl::string_view input) {
-  absl::flat_hash_set<int> result;
+  absl::flat_hash_set<int> suppress_tokens;
 
   for (absl::string_view s :
        absl::StrSplit(input, ',', absl::SkipWhitespace())) {
     int val;
     if (absl::SimpleAtoi(s, &val)) {
-      result.insert(val);
+      suppress_tokens.insert(val);
     }
   }
 
-  return ::litert::lm::SuppressTokensConfig(result);
+  return ::litert::lm::SuppressTokensConfig(std::move(suppress_tokens));
 }
 
 // Writes the metrics to the given file path in protobuf format. Only used in
@@ -226,6 +234,8 @@ absl::Status MainHelper(int argc, char** argv) {
            "[--presence_penalty=<presence_penalty>]"
            "[--frequency_penalty=<frequency_penalty>]"
            "[--repetition_window_size=<repetition_window_size>]"
+           "[--no_repeat_ngram_size=<no_repeat_ngram_size>]"
+           "[--no_repeat_ngram_window_size=<no_repeat_ngram_window_size>]"
            "[--suppress_tokens=<token1,token2,...>]"
            "[--constraint_regex=<constraint_regex>]"
            "[--enable_speculative_decoding=<true|false>]";
@@ -308,6 +318,7 @@ absl::Status MainHelper(int argc, char** argv) {
       : absl::GetFlag(FLAGS_conv_type) == "int8" ? litert::lm::ConvType::kInt8
                                                  : litert::lm::ConvType::kAuto;
   settings.repetition_penalty_config = GetRepetitionPenaltyConfig();
+  settings.no_repeat_ngram_config = GetNoRepeatNgramConfig();
   settings.suppress_tokens_config =
       GetSuppressTokensConfig(absl::GetFlag(FLAGS_suppress_tokens));
   settings.constraint_regex = absl::GetFlag(FLAGS_constraint_regex);
